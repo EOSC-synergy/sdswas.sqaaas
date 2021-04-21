ee966ec71171b34549066a3bbff0c2e55c48de6d
Francesco Benincasa <francesco.benincasa@bsc.es>
2021-04-22 01:58:48 +0200

    Preproc for scores improved

--- preproc/nc2timeseries_scores.py
+++ preproc/nc2timeseries_scores.py
@@ -47,19 +47,32 @@ def preprocess(ds, n=8):
 
 
 def ret_scores(mod, obs, flt, st):
-    print('STATION', st)
     flt_stat = flt.get_group(st).set_index('time')
     try:
-        obs_stat = obs.get_group(st).set_index('time')[flt_stat[OBS[obs]['flt_var']]>0.6]
+        obs_stat = obs.get_group(st).set_index('time')[flt_stat[OBS['aeronet']['flt_var']]>0.6]
     except:
+        return flt_stat['station_name'][0], {'BIAS':  np.nan, 'CORR': np.nan, 'RMSE': np.nan, 'FRGE': np.nan}
+    if obs_stat.size == 0:
         return flt_stat['station_name'][0], {'BIAS':  np.nan, 'CORR': np.nan, 'RMSE': np.nan, 'FRGE': np.nan}
     mod_stat = mod.get_group(st).set_index('time')
     tot = obs_stat.merge(mod_stat, on='time')
-    mvar, ovar = OBS[obs]['mod_var'], OBS[obs]['obs_var']
-    bias = (tot[mvar] - tot[ovar]).mean()
-    corr = tot[[ovar, mvar]].corr()
-    rmse = mean_squared_error(tot[ovar], tot[mvar], squared=False)
-    frge = 2*((tot[mvar] - tot[ovar])/(tot[mvar] + tot[ovar])).abs().mean()
+    mvar, ovar = OBS['aeronet']['mod_var'], OBS['aeronet']['obs_var']
+    try:
+        bias = (tot[mvar] - tot[ovar]).mean()
+    except:
+        bias = np.nan
+    try:
+        corr = tot[[ovar, mvar]].corr()
+    except:
+        corr = np.nan
+    try:
+        rmse = mean_squared_error(tot[ovar], tot[mvar], squared=False)
+    except:
+        rmse = np.nan
+    try:
+        frge = 2*((tot[mvar] - tot[ovar])/(tot[mvar] + tot[ovar])).abs().mean()
+    except:
+        frge = np.nan
     return obs_stat['station_name'][0], {'BIAS':  bias, 'CORR': corr.values[0][1], 'RMSE': rmse, 'FRGE': frge}
 
 
@@ -96,6 +109,7 @@ def convert2timeseries(model, obs=None, months=None):
     print("FLT", flt_paths)
 
     columns = ['station'] + list(MODELS.keys())
+    print(columns)
     bias_df = pd.DataFrame(columns=columns)
     rmse_df = pd.DataFrame(columns=columns)
     corr_df = pd.DataFrame(columns=columns)
@@ -104,107 +118,108 @@ def convert2timeseries(model, obs=None, months=None):
     sites = pd.read_table('/home/fbeninca/devel/interactive-forecast-viewer/conf/aeronet_sites.txt', delimiter=r"\s+", engine='python')
 
     # for mpath, opath, fpath, month in zip(*mod_paths, obs_paths, flt_paths, months):
-    for outputs in zip(*mod_paths, obs_paths, flt_paths, months):
-        models_ds = []
-        for mpath in outputs[:-3]:
-            fnames = glob(mpath)
-            if not fnames:
-                print('No files correspondence to path', mpath)
-                continue
-
-            try:
-                mod_ds = xr.open_mfdataset(mpath,
-                                       concat_dim='time',
-                                       combine='nested',
-                                       preprocess=preprocess)
-                models_ds.append(mod_ds)
-            except Exception as err:
-                print('Error', str(err))
-                continue
-
-
-        opath = outputs[-3]
-        fpath = outputs[-2]
-
+    # for outputs in zip(*mod_paths, obs_paths, flt_paths, months):
+    models_ds = []
+    for mpath in mod_paths:
+        fnames = [x for mp in mpath for x in sorted(glob(mp))]
         try:
-            obs_ds = None
-            flt_ds = None
-            if opath:
-                obs_ds = xr.open_dataset(opath)
-                obs_ds = obs_ds.where(obs_ds['station_name'].isin(sites['SITE'].values.astype(np.bytes_)))
-                obs_df = obs_ds.to_dataframe().reset_index().dropna(subset=['station_name'])
-                obs_df = obs_df[['time', 'station', 'ndata', 'station_name', OBS[obs]['obs_var']]]
-                old_stations = obs_df['station'].unique()
-                new_stations = np.arange(old_stations.size)
-                for old_st, new_st in zip(old_stations, new_stations):
-                    obs_df.loc[obs_df['station'] == old_st, 'station'] = new_st
-                obs_grps = obs_df.groupby('station')
-            if fpath:
-                flt_ds = xr.open_dataset(fpath)
-                flt_ds = flt_ds.where(flt_ds['station_name'].isin(sites['SITE'].values.astype(np.bytes_)))
-                flt_df = flt_ds.to_dataframe().reset_index().dropna(subset=['station_name'])
-                flt_df = flt_df[['time', 'station', 'ndata', 'station_name', OBS[obs]['flt_var']]]
-                for old_st, new_st in zip(old_stations, new_stations):
-                    flt_df.loc[flt_df['station'] == old_st, 'station'] = new_st
-                flt_grps = flt_df.groupby('station')
+            #print(fnames)
+            mod_ds = xr.open_mfdataset(fnames,
+                                   concat_dim='time',
+                                   combine='nested',
+                                   preprocess=preprocess)
+            models_ds.append(mod_ds)
         except Exception as err:
             print('Error', str(err))
             continue
 
-        for variable in VARS:
-            print('v', variable)
-            # if observation perform interpolation
-            print('mod var', OBS[obs]['mod_var'])
-            print('obs var', OBS[obs]['obs_var'])
+    try:
+        obs_ds = xr.open_mfdataset(obs_paths,
+                               concat_dim='time',
+                               combine='nested',
+                               )
+        obs_ds = obs_ds.where(obs_ds['station_name'].isin(sites['SITE'].values.astype(np.bytes_)))
+        obs_df = obs_ds.to_dataframe().reset_index().dropna(subset=['station_name'])
+        obs_df = obs_df[['time', 'station', 'ndata', 'station_name', OBS[obs]['obs_var']]]
+        old_stations = obs_df['station'].unique()
+        new_stations = np.arange(old_stations.size)
+        for old_st, new_st in zip(old_stations, new_stations):
+            obs_df.loc[obs_df['station'] == old_st, 'station'] = new_st
+        obs_lat = obs_ds['latitude'].dropna('station')[0].drop_vars('time')
+        obs_lon = obs_ds['longitude'].dropna('station')[0].drop_vars('time')
+        obs_grps = obs_df.groupby('station')
+        flt_ds = xr.open_mfdataset(flt_paths,
+                               concat_dim='time',
+                               combine='nested',
+                               )
+        flt_ds = flt_ds.where(flt_ds['station_name'].isin(sites['SITE'].values.astype(np.bytes_)))
+        flt_df = flt_ds.to_dataframe().reset_index().dropna(subset=['station_name'])
+        flt_df = flt_df[['time', 'station', 'ndata', 'station_name', OBS[obs]['flt_var']]]
+        for old_st, new_st in zip(old_stations, new_stations):
+            flt_df.loc[flt_df['station'] == old_st, 'station'] = new_st
+        flt_grps = flt_df.groupby('station')
+    except Exception as err:
+        print('Error', str(err))
 
-            for mod_ds in models_ds:
-                stations = []
-                bias = []
-                rmse = []
-                corr = []
-                frge = []
+    variable = OBS[obs]['mod_var']
+    print('v', variable)
+    # if observation perform interpolation
+    print('mod var', OBS[obs]['mod_var'])
+    print('obs var', OBS[obs]['obs_var'])
 
-                if variable in mod_ds.variables:
-                    if obs_ds:
-                        obs_lat = obs_ds['latitude'].dropna('station')
-                        obs_lon = obs_ds['longitude'].dropna('station')
-                        print('interpolating to observations ...')
-                        try:
-                            mod_interp = mod_ds[variable].interp(longitude=obs_lon, latitude=obs_lat)
-                        except:
-                            mod_interp = mod_ds[variable].interp(lon=obs_lon, lat=obs_lat)
-                        print('converting to df  ...')
-                    else:
-                        mod_interp = mod_ds[variable]
-                        print('converting dataset to df ...')
+    print(models_ds)
+    for mod_idx, mod_ds in enumerate(models_ds):
+        stations = []
+        bias = []
+        rmse = []
+        corr = []
+        frge = []
+        print('IDX', mod_idx)
+        if variable in mod_ds.variables:
+            if obs_ds:
+                print('interpolating to observations ...')
+                try:
+                    mod_interp = mod_ds[variable].interp(longitude=obs_lon, latitude=obs_lat)
+                except:
+                    mod_interp = mod_ds[variable].interp(lon=obs_lon, lat=obs_lat)
+                print('converting to df  ...')
+            else:
+                mod_interp = mod_ds[variable]
+                print('converting dataset to df ...')
 
-                    mod_df = mod_interp.to_dataframe().reset_index()
-                    mod_grps = mod_df.groupby('station')
+            mod_df = mod_interp.to_dataframe().reset_index()
+            mod_grps = mod_df.groupby('station')
 
-                    for grp in mod_grps:
-                        station, scores = ret_scores(mod_grps, obs_grps, flt_grps, grp[0])
-                        stations.append(station)
-                        bias.append(scores['BIAS'])
-                        rmse.append(scores['RMSE'])
-                        corr.append(scores['CORR'])
-                        frge.append(scores['FRGE'])
+            for grp in mod_grps:
+                station, scores = ret_scores(mod_grps, obs_grps, flt_grps, grp[0])
+                stations.append(station)
+                bias.append(scores['BIAS'])
+                rmse.append(scores['RMSE'])
+                corr.append(scores['CORR'])
+                frge.append(scores['FRGE'])
 
-                bias_df['station'] = stations
-                bias_df[columns[models_ds.index(mod_ds)+1]] = bias
-                rmse_df['station'] = stations
-                rmse_df[columns[models_ds.index(mod_ds)+1]] = rmse
-                corr_df['station'] = stations
-                corr_df[columns[models_ds.index(mod_ds)+1]] = corr
-                frge_df['station'] = stations
-                frge_df[columns[models_ds.index(mod_ds)+1]] = frge
+        print(stations)
+        print(columns[mod_idx+1])
+        print('BIAS', bias)
+        print('RMSE', rmse)
+        print('CORR', corr)
+        print('FRGE', frge)
+        bias_df['station'] = stations
+        bias_df[columns[mod_idx+1]] = bias
+        rmse_df['station'] = stations
+        rmse_df[columns[mod_idx+1]] = rmse
+        corr_df['station'] = stations
+        corr_df[columns[mod_idx+1]] = corr
+        frge_df['station'] = stations
+        frge_df[columns[mod_idx+1]] = frge
 
-        if obs_ds:
-            obs_ds.close()
-        if flt_ds:
-            flt_ds.close()
-        mod_ds.close()
+    if obs_ds:
+        obs_ds.close()
+    if flt_ds:
+        flt_ds.close()
+    mod_ds.close()
 
-        print('BIAS', bias_df)
+    print('BIAS', bias_df)
 
 
 if __name__ == "__main__":
